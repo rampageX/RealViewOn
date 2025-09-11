240d55ae953305285cf3e1592997d143d674f261
Ian Bassi <ian.bassi@outlook.com>
2025-09-11 17:15:29 -0300

    Bump 2.1.0.0

--- include/Version.h
+++ include/Version.h
@@ -2,10 +2,10 @@
 #define VERSION_H
 
 #define FILE_VERSION_MAJOR 2
-#define FILE_VERSION_MINOR 0
-#define FILE_VERSION_PATCH 9
-#define FILE_VERSION_BUILD 8
-#define FILE_VERSION_STABLE 0 // 1 for stable, 0 for prerelease
+#define FILE_VERSION_MINOR 1
+#define FILE_VERSION_PATCH 0
+#define FILE_VERSION_BUILD 0
+#define FILE_VERSION_STABLE 1 // 1 for stable, 0 for prerelease
 
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
