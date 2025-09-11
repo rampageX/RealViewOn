f66f25f0404411b320468343a996e08a57547f7a
Ian Bassi <ian.bassi@outlook.com>
2025-09-11 16:40:24 -0300

    Test Release before 2.1

--- include/Version.h
+++ include/Version.h
@@ -4,7 +4,7 @@
 #define FILE_VERSION_MAJOR 2
 #define FILE_VERSION_MINOR 0
 #define FILE_VERSION_PATCH 9
-#define FILE_VERSION_BUILD 7
+#define FILE_VERSION_BUILD 8
 #define FILE_VERSION_STABLE 0 // 1 for stable, 0 for prerelease
 
 #define STRINGIFY(x) #x
